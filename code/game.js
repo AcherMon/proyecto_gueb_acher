ba6624a3b2170cc22e9a72041cbc80c61336e87d
Achermon <achermontuenga2008@gmail.com>
2025-11-06 22:36:24 +0100

    NO SE AVANZAR

--- code/game.js
+++ code/game.js
@@ -1,5 +1,22 @@
-function check(){
-    if(document.getElementById("web").value=="MO"){
-        alert(Hola)
+function check(event){
+    if (event) event.preventDefault();
+
+    if(document.getElementById("web").value == "MO"){
+        document.getElementById("recuadro").innerHTML += '<img src="../media/bien.png" alt="Respuesta correcta" style="width:150px; display:block; margin:10px auto;">';
+    }
+    else{
+        document.getElementById("recuadro").innerHTML += '<img src="../media/mal.png" alt="Respuesta incorrecta" style="width:150px; display:block; margin:10px auto;">';
+    }
+}
+
+
+function check(event){
+    if (event) event.preventDefault();
+
+    if(document.getElementById("que").value == "CC"){
+        document.getElementById("2.0").innerHTML += '<img src="../media/bien.png" alt="Respuesta correcta" style="width:150px; display:block; margin:10px auto;">';
+    }
+    else{
+        document.getElementById("2.0").innerHTML += '<img src="../media/mal.png" alt="Respuesta incorrecta" style="width:150px; display:block; margin:10px auto;">';
     }
 }
