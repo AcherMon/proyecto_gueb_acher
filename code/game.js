1bc1f764c2b867c0b4d6fa171de2b802eed9efc7
Clase <200025@iessanalberto.com>
2025-11-07 09:19:42 +0100

    VAYA MIERDON

--- code/game.js
+++ code/game.js
@@ -1,22 +1,10 @@
 function check(event){
     if (event) event.preventDefault();
 
-    if(document.getElementById("web").value == "MO"){
-        document.getElementById("recuadro").innerHTML += '<img src="../media/bien.png" alt="Respuesta correcta" style="width:150px; display:block; margin:10px auto;">';
+    if(document.getElementById("primera").value == "MO"){
+        document.getElementById("I1").innerHTML += '<img src="../media/bien.png" alt="Respuesta correcta">';
     }
     else{
-        document.getElementById("recuadro").innerHTML += '<img src="../media/mal.png" alt="Respuesta incorrecta" style="width:150px; display:block; margin:10px auto;">';
-    }
-}
-
-
-function check(event){
-    if (event) event.preventDefault();
-
-    if(document.getElementById("que").value == "CC"){
-        document.getElementById("2.0").innerHTML += '<img src="../media/bien.png" alt="Respuesta correcta" style="width:150px; display:block; margin:10px auto;">';
-    }
-    else{
-        document.getElementById("2.0").innerHTML += '<img src="../media/mal.png" alt="Respuesta incorrecta" style="width:150px; display:block; margin:10px auto;">';
+        document.getElementById("I1").innerHTML += '<img src="../media/mal.png" alt="Respuesta incorrecta">';
     }
 }
